9c26e2856a5cd2092baf63416d9f1a55f81ea307
Sergey Aristov <aristov775@gmail.com>
2022-04-22 15:20:02 +0300

    Add buildSrc, dependencies and versions.

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    compileSdk = AppConfig.completeSdk
+
+    defaultConfig {
+        applicationId = AppConfig.applicationId
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        versionCode = Releases.versionCode
+        versionName = Releases.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = AppConfig.javaVersion
+        targetCompatibility = AppConfig.javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = AppConfig.jvmTarget
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.Compose.core
+    }
+    packagingOptions {
+        resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.Jetpack.core)
+    implementation(Dependencies.JetpackCompose.ui)
+    implementation(Dependencies.JetpackCompose.material)
+    implementation(Dependencies.JetpackCompose.uiTooling)
+    implementation(Dependencies.JetpackCompose.activity)
+    implementation(Dependencies.Jetpack.lifeCycle)
+
+    testImplementation(Dependencies.Test.junit)
+    androidTestImplementation(Dependencies.Test.junitExt)
+    androidTestImplementation(Dependencies.Test.espresso)
+    androidTestImplementation(Dependencies.JetpackCompose.junit)
+    debugImplementation(Dependencies.JetpackCompose.uiTooling)
+}
