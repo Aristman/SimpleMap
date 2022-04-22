954a1c2f1b749756620ea81f2346990d5b153dce
Sergey Aristov <aristov775@gmail.com>
2022-04-22 14:08:49 +0300

    init commit

---
+++ app/src/main/java/ru/marslab/simplemap/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.marslab.simplemap
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import ru.marslab.simplemap.ui.theme.SimpleMapTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleMapTheme {
+            }
+        }
+    }
+}
